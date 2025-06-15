249769ff6a21955bfb24ba7cacc5766ee2427a12
EZiemniak <eric29ziem@gmail.com>
2025-06-15 03:24:00 -0600

    fix: Convert NaN's to None's for valid JSON + Allows frontend to directly fetch valid json with no errors

--- scripts/cache_players.py
+++ scripts/cache_players.py
@@ -8,6 +8,17 @@ import pandas as pd
 from tqdm import tqdm
 from config import ACTIVE_PLAYERS_UPDATE, LOGS_DIR, ACTIVE_PLAYERS_FILE, RETIRED_PLAYERS_FILE, CACHED_IDS_FILE, TIME_BETWEEN_UPDATES
    
+""" Recursively convert NaN values to None for a list of dicts"""
+def convert_nans_to_none(obj):
+    if isinstance(obj, dict):
+        return {k: convert_nans_to_none(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [convert_nans_to_none(item) for item in obj]
+    elif isinstance(obj, float) and pd.isna(obj):
+        return None
+    else:
+        return obj
+    
 def get_item(val):
     return val.item() if hasattr(val, 'item') else None
     
@@ -26,9 +37,13 @@ def get_player_info(player: dict) -> None:
             return
     player_info = commonplayerinfo.CommonPlayerInfo(player_id=player['id'])
     career = playercareerstats.PlayerCareerStats(per_mode36="PerGame", player_id=player['id'])
-    season_by_season_stats = career.get_data_frames()[0]
-    career_averages = career.get_data_frames()[1]
-    player_info_df = player_info.get_data_frames()[0]
+    
+    raw_career_averages_df = career.get_data_frames()[1]
+    raw_player_info_df = player_info.get_data_frames()[0]
+
+    season_by_season_stats_df = career.get_data_frames()[0]
+    career_averages_df = raw_career_averages_df.where(pd.notnull(raw_career_averages_df), None)
+    player_info_df = raw_player_info_df.where(pd.notnull(raw_player_info_df), None)
 
     player['info'] = {
       'height': player_info_df['HEIGHT'].values[0],
@@ -51,34 +66,34 @@ def get_player_info(player: dict) -> None:
       'greatest_75_flag': player_info_df['GREATEST_75_FLAG'].values[0],
       'nba_flag': player_info_df['NBA_FLAG'].values[0],
       'dleague_flag': player_info_df['DLEAGUE_FLAG'].values[0], 
-      'teams_list': season_by_season_stats.query('TEAM_ABBREVIATION != "TOT"')['TEAM_ABBREVIATION'].unique().tolist()
+      'teams_list': season_by_season_stats_df.query('TEAM_ABBREVIATION != "TOT"')['TEAM_ABBREVIATION'].unique().tolist()
     }
 
     player['career_stats'] = {
-        'games_started': get_item(career_averages['GS'].values[0]),
-        'games_played': get_item(career_averages['GP'].values[0]),
-        'minutes_avg': get_item(career_averages['MIN'].values[0]),
-        'fgm_avg': get_item(career_averages['FGM'].values[0]),
-        'fga_avg': get_item(career_averages['FGA'].values[0]),
-        'fg_pct': get_item(career_averages['FG_PCT'].values[0]),
-        '3pm_avg': get_item(career_averages['FG3M'].values[0]),
-        '3pa_avg': get_item(career_averages['FG3A'].values[0]),
-        '3p_pct': get_item(career_averages['FG3_PCT'].values[0]),
-        'ftm_avg': get_item(career_averages['FTM'].values[0]),
-        'fta_avg': get_item(career_averages['FTA'].values[0]),
-        'ft_pct': get_item(career_averages['FT_PCT'].values[0]),
-        'orb_avg': get_item(career_averages['OREB'].values[0]),
-        'drb_avg': get_item(career_averages['DREB'].values[0]),
-        'rpg': get_item(career_averages['REB'].values[0]),
-        'apg': get_item(career_averages['AST'].values[0]),
-        'spg': get_item(career_averages['STL'].values[0]),
-        'bpg': get_item(career_averages['BLK'].values[0]),
-        'tpg': get_item(career_averages['TOV'].values[0]),
-        'ppg': get_item(career_averages['PTS'].values[0]),
-        'pf': get_item(career_averages['PF'].values[0])
+        'games_started': get_item(career_averages_df['GS'].values[0]),
+        'games_played': get_item(career_averages_df['GP'].values[0]),
+        'minutes_avg': get_item(career_averages_df['MIN'].values[0]),
+        'fgm_avg': get_item(career_averages_df['FGM'].values[0]),
+        'fga_avg': get_item(career_averages_df['FGA'].values[0]),
+        'fg_pct': get_item(career_averages_df['FG_PCT'].values[0]),
+        '3pm_avg': get_item(career_averages_df['FG3M'].values[0]),
+        '3pa_avg': get_item(career_averages_df['FG3A'].values[0]),
+        '3p_pct': get_item(career_averages_df['FG3_PCT'].values[0]),
+        'ftm_avg': get_item(career_averages_df['FTM'].values[0]),
+        'fta_avg': get_item(career_averages_df['FTA'].values[0]),
+        'ft_pct': get_item(career_averages_df['FT_PCT'].values[0]),
+        'orb_avg': get_item(career_averages_df['OREB'].values[0]),
+        'drb_avg': get_item(career_averages_df['DREB'].values[0]),
+        'rpg': get_item(career_averages_df['REB'].values[0]),
+        'apg': get_item(career_averages_df['AST'].values[0]),
+        'spg': get_item(career_averages_df['STL'].values[0]),
+        'bpg': get_item(career_averages_df['BLK'].values[0]),
+        'tpg': get_item(career_averages_df['TOV'].values[0]),
+        'ppg': get_item(career_averages_df['PTS'].values[0]),
+        'pf': get_item(career_averages_df['PF'].values[0])
     }
         
-    player['season_by_season_stats'] = season_by_season_stats.to_dict(orient='records')
+    player['season_by_season_stats'] = convert_nans_to_none(season_by_season_stats_df.to_dict(orient='records'))
       
     player['awards'] = playerawards.PlayerAwards(player_id=player['id']).get_dict()
     
@@ -194,10 +209,10 @@ if __name__ == "__main__":
         with open(CACHED_IDS_FILE, "w") as f_ids:
             json.dump(cached_ids, f_ids)
         with open(ACTIVE_PLAYERS_FILE, "w", encoding='utf-8') as f_active:
-            json.dump(active_players, f_active, ensure_ascii=False, indent=2)
+            json.dump(active_players, f_active, ensure_ascii=False, allow_nan=False, indent=2)
         if not ACTIVE_PLAYERS_UPDATE:
             with open(RETIRED_PLAYERS_FILE, "w", encoding='utf-8') as f_retired:
-                json.dump(retired_players, f_retired, ensure_ascii=False, indent=2)
+                json.dump(retired_players, f_retired, ensure_ascii=False, allow_nan=False, indent=2)
     except Exception as e:
         print(f'Error: Failed to save cached IDs or player lists: {e}\nTry deleting JSON files and run again.')
         sys.exit(1)
